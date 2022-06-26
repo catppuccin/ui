418f95c5361fda2662db3e4d32bc7f4809727ac5
Andreas Ekström <didair@msn.com>
2022-06-26 02:09:55 +0200

    Add anchor component

---
+++ src/components/Anchor/index.js
@@ -0,0 +1,44 @@
+import React, { createElement } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+export const Anchor = ({ as, underline, ...props }) => {
+
+	const anchorProps = {
+		...props,
+		className: cx(
+			'text-rosewater',
+			{
+				'decoration-lavender': underline != 'never',
+				'decoration-2': underline != 'never',
+				'hover:underline': underline == 'hover',
+				'underline': underline == 'always',
+			}
+		),
+	};
+
+	return createElement(as ?? 'a', anchorProps, props.children);
+};
+
+Anchor.propTypes = {
+	/**
+	 * Configure element to be used for rendering anchor tag
+	 */
+	as: PropTypes.oneOfType([
+		PropTypes.element,
+		PropTypes.oneOf('a'),
+	]),
+	/**
+	 * Configure when underline should be shown
+	 */
+	underline: PropTypes.oneOf([
+		'never',
+		'hover',
+		'always',
+	]),
+};
+
+Anchor.defaultProps = {
+	underline: 'hover',
+	as: 'a',
+};
---
+++ src/stories/Anchor.stories.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Anchor } from 'components/Anchor';
+
+export default {
+	title: 'Components/Anchor',
+	component: Anchor,
+};
+
+const Template = (args) => <Anchor
+	onClick={(e) => e.preventDefault()}
+	{...args}
+/>;
+
+export const Preview = Template.bind({});
+Preview.args = {
+	children: 'Link content',
+	href: '#yes'
+};
