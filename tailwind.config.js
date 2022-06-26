7f9a3e9800de8fef8fbfb7a4002f099623880f46
Andreas Ekström <didair@msn.com>
2022-06-26 02:08:03 +0200

    Update tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,26 @@
-// import colors from './scripts/colors';
-// import palettes from '@catppuccin/palette';
-
 const colors = require('./scripts/colors.js');
-
-console.log('colors tailwind', colors);
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-	colors: () => colors.getColors(),
-  },
-  plugins: [],
-}
+	content: [
+		'./public/index.html',
+		'./src/**/*.{html,js}',
+	],
+	theme: {
+		extend: {},
+		colors: () => colors.getColors(),
+		borderRadius: {
+			none: '0px',
+			sm: '0.125rem',
+			DEFAULT: '0.25rem',
+			md: '0.375rem',
+			lg: '0.5rem',
+			xl: '0.75rem',
+			'2xl': '1rem',
+			'3xl': '1.5rem',
+			full: '9999px',
+		},
+	},
+
+	plugins: [],
+};
