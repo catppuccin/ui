15291c35983c22aac6293a23821c418d6e79c896
Andreas Ekström <didair@msn.com>
2022-07-02 02:47:36 +0200

    Add global exports for List and Paragraph

--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@ export * from './components/Avatar';
 export * from './components/Button';
 export * from './components/Heading';
 export * from './components/Input';
+export * from './components/List';
+export * from './components/Paragraph';
