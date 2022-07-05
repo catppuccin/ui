8552507ac2e10bf6f1ba71d6c0a3a8faf7d33a5f
Andreas Ekström <didair@msn.com>
2022-07-05 22:52:47 +0200

    WIP colors constants

---
+++ src/constants/colors.js
@@ -0,0 +1,36 @@
+export const backgrounds = [
+	'base',
+	'crust',
+	'mantle',
+	'surface0',
+	'surface1',
+	'surface1',
+	'overlay0',
+	'overlay1',
+	'overlay2',
+];
+
+export const text = [
+	'text',
+	'subtext0',
+	'overlay1',
+	'rosewater',
+	'green',
+	'peach',
+	'maroon',
+	'blue',
+];
+
+export const highlights = [
+	'text-text',
+	'text-subtext0',
+	'text-overlay1',
+	'red',
+	'green',
+	'yellow',
+	'blue',
+	'pink',
+	'teal',
+	'peach',
+	'rosewater',
+];
