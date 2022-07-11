0537edf31ae7787b245e5b2f31932791a9fe9901
Andreas Ekström <didair@msn.com>
2022-07-11 04:05:54 +0200

    Use useMemo hook to avoid element getting new id every re-render

--- src/libs/id.js
+++ src/libs/id.js
@@ -1,12 +1,15 @@
+import { useMemo } from 'react';
 import { unique } from "./shorthash";
 
 /**
  * @param {string} prefix
  */
 export const useId = (prefix = null) => {
-	const hash = unique((Math.random().toString(36)+'00000000000000000').slice(2, 7));
+	return useMemo(() => {
+		const hash = unique((Math.random().toString(36)+'00000000000000000').slice(2, 7));
 
-	return prefix != null ?
-		`${prefix}-${hash}`
-	: hash;
+		return prefix != null ?
+			`${prefix}-${hash}`
+		: hash;
+	}, []);
 };
